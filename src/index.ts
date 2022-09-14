bc526b8338249ca695f5dbb78513d994b624944f
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-09-14 17:38:14 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export const mypackage = () => {
-  return 'yo';
+export function helloWorld() {
+  const message = 'Hello World from my example modern npm package!';
+  return message;
+}
+
+export function goodBye() {
+  const message = 'Goodbye from my example modern npm package!';
+  return message;
+}
+
+export default {
+  helloWorld,
+  goodBye,
 };
